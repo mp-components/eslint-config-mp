1a31caa920327358f07bef395b8b8c5502c82b87
pengtikui <949828390@qq.com>
2019-05-31 16:19:38 +0800

    update v0.1.0

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 module.exports = {
   root: true,
 
+  extends: ['airbnb-base'],
+
   env: {
     browser: false,
     commonjs: true,
     es6: true,
     node: true,
-  },
-
-  parserOptions: {
-    ecmaVersion: 6,
-    sourceType: 'module',
   },
 
   globals: {
@@ -23,35 +20,5 @@ module.exports = {
     'getRegExp': true,
     'getDate': true,
     'Behavior': true
-  },
-
-  plugins: ['import'],
-
-  settings: {
-    'import/extensions': ['.js']
-  },
-
-  rules: {
-    // https://github.com/benmosher/eslint-plugin-import/tree/master/docs/rules
-    'import/no-unresolved': ['error', { commonjs: true, caseSensitive: true }],
-    'import/named': 'error',
-    'import/default': 'off',
-    'import/export': 'error',
-    'import/no-named-as-default': 'error',
-    'import/no-named-as-default-member': 'error',
-    'import/no-mutable-exports': 'error',
-    'import/no-amd': 'error',
-    'import/first': 'error',
-    'import/no-duplicates': 'error',
-    'import/order': ['error', { groups: [['builtin', 'external', 'internal']] }],
-    'import/newline-after-import': 'error',
-    'import/prefer-default-export': 'error',
-    'import/no-absolute-path': 'error',
-    'import/no-dynamic-require': 'error',
-    'import/no-webpack-loader-syntax': 'error',
-    'import/no-named-default': 'error',
-    'import/no-self-import': 'error',
-    'import/no-cycle': ['error', { maxDepth: Infinity }],
-    'import/no-useless-path-segments': 'error',
   },
 };
